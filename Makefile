2cf2d843aca5e972a3c90f423efc08eca486f8b5
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-04 11:04:06 +0800

    update Makefile and prj dir structure

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BUILD_HOME:=./build
+IP_HOME:=./ip
 
 MYCPU_HOME:=./mycpu_env/myCPU
 MYCPU_SRC:=$(shell find $(MYCPU_HOME) -name "*.v")
@@ -8,21 +8,18 @@ MYCPU_IP:=$(shell find $(MYCPU_HOME) -name "*.xci")
 SOC_AXI_HOME:=./mycpu_env/soc_verify/soc_axi/rtl
 CONFREG_SRC:=$(SOC_AXI_HOME)/CONFREG/confreg.v
 
-MYCPU_IP_HOME:=$(BUILD_HOME)/mycpu
-IP_REPO_HOME:=$(BUILD_HOME)/ip_repo
+MYCPU_IP_HOME:=$(IP_HOME)/mycpu
+CONFREG_IP_HOME:=$(IP_HOME)/confreg
 
-.PHONY: clean mycpu_ip ip_repo
+.PHONY: clean mycpu_ip
 
 mycpu_ip: $(MYCPU_SRC) $(MYCPU_IP)
 	@rm -rf $(MYCPU_IP_HOME)
 	@vivado -mode tcl -source ./scripts/package_ip.tcl -notrace
 
 confreg_ip: $(CONFREG_SRC)
+	@rm -rf $(CONFREG_IP_HOME)
 	@vivado -mode tcl -source ./scripts/package_ip_confreg.tcl -notrace
 
-ip_repo: mycpu_ip
-	@mkdir -p $(IP_REPO_HOME)
-	@cp $(MYCPU_IP_HOME)/*.zip $(IP_REPO_HOME)
-
 clean:
-	rm -rf ./vivado* $(IP_REPO_HOME)
+	rm -rf ./vivado*
--- scripts/package_ip.tcl
+++ scripts/package_ip.tcl
@@ -1,4 +1,4 @@
-create_project -force mycpu ./build/mycpu -part xc7a200tfbg676-1
+create_project -force ./ip/mycpu ./ip/mycpu -part xc7a200tfbg676-1
 
 # Add conventional sources
 # add_files -scan_for_includes ../rtl
@@ -20,9 +20,9 @@ update_compile_order -fileset sources_1
 # set_property -name "top" -value "tb_top" -objects  [get_filesets sim_1]
 # set_property -name "xsim.simulate.log_all_signals" -value "1" -objects [get_filesets sim_1]
 
-ipx::package_project -root_dir /home/haooops/Documents/cdp_ede_local/build/mycpu -vendor user.org -library user -taxonomy /UserIP -import_files -set_current false -force
-ipx::unload_core /home/haooops/Documents/cdp_ede_local/build/mycpu/component.xml
-ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory /home/haooops/Documents/cdp_ede_local/build/mycpu /home/haooops/Documents/cdp_ede_local/build/mycpu/component.xml
+ipx::package_project -root_dir /home/haooops/Documents/cdp_ede_local/ip/mycpu -vendor user.org -library user -taxonomy /UserIP -import_files -set_current false -force
+ipx::unload_core /home/haooops/Documents/cdp_ede_local/ip/mycpu/component.xml
+ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory /home/haooops/Documents/cdp_ede_local/ip/mycpu /home/haooops/Documents/cdp_ede_local/ip/mycpu/component.xml
 
 update_compile_order -fileset sources_1
 ipx::add_bus_parameter FREQ_HZ [ipx::get_bus_interfaces interface_aximm -of_objects [ipx::current_core]]
@@ -33,11 +33,11 @@ ipx::create_xgui_files [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
 ipx::save_core [ipx::current_core]
 ipx::check_integrity -quiet [ipx::current_core]
-ipx::archive_core /home/haooops/Documents/cdp_ede_local/build/mycpu/user.org_user_mycpu_top_1.0.zip [ipx::current_core]
+ipx::archive_core /home/haooops/Documents/cdp_ede_local/ip/mycpu/user.org_user_mycpu_top_1.0.zip [ipx::current_core]
 ipx::move_temp_component_back -component [ipx::current_core]
 close_project -delete
 
-set_property  ip_repo_paths  {/home/haooops/Documents/cdp_ede_local/build/mycpu /home/haooops/Documents/cdp_ede_local/build/ip_repo} [current_project]
+set_property  ip_repo_paths  {/home/haooops/Documents/cdp_ede_local/ip/mycpu /home/haooops/Documents/cdp_ede_local/ip/ip_repo} [current_project]
 update_ip_catalog
 
 exit
